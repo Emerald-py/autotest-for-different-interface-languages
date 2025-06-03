9a4c3ca86ba17fcc3d8d8e8097c9f3c2f27f4288
Kirill <maliginkirill@yandex.ru>
2025-06-03 12:59:55 +0300

    added assert

--- test_items.py
+++ test_items.py
@@ -1,10 +1,9 @@
-import pytest
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
 
 def test_basket_button(browser):
     browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
-    button = browser.find_element(By.CLASS_NAME, "btn-add-to-basket")
-    time.sleep(5)
+    time.sleep(30)
+    button = browser.find_elements(By.CLASS_NAME, "btn-add-to-basket")
+    assert len(button) == 1, "The button is missing"
